d8050ad04b1055f45e1a813da59e8448ac9fd401
mohamednasermhst <mohamed.naser.mhs@icloud.com>
2026-09-03 09:07:24 +0400

    V4

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
    activate handler below delete the previous cache -- without a bump an
    installed app can keep serving old files even though the server has
    new ones. */
-var CACHE = "household-budget-v43";
+var CACHE = "household-budget-v45";
 
 self.addEventListener("install", function(){ self.skipWaiting(); });
 
