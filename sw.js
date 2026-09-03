d1cb662dcb2363abe33ce4516b4f3f6e3d200857
mohamednasermhst <mohamed.naser.mhs@icloud.com>
2026-09-03 20:04:55 +0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
    activate handler below delete the previous cache -- without a bump an
    installed app can keep serving old files even though the server has
    new ones. */
-var CACHE = "household-budget-v65";
+var CACHE = "household-budget-v68";
 
 self.addEventListener("install", function(){ self.skipWaiting(); });
 
